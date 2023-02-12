f8782f4cd8204d9b305592ff043036f5bea146c2
Bulletshell <joeyjordison619@gmail.com>
2023-02-12 11:43:14 +0700

    First commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("Hello World!")
+
+let x = 10
+let nama = "Reihan"
+x = 20
+
+console.log(x);
+console.log(nama);
+
+const PI = 3.14
+//PI=5
+console.log(PI)
+
+let var1=10
+let var2=10
---
+++ object.js
@@ -0,0 +1,17 @@
+let data = {
+    "nama" : "M Reihan S",
+    "umur" : 24,
+    "alamat" : "Jakarta Selatan",
+    "pekerjaan" : "Fresh Graduate"
+}
+
+console.log(data)
+
+let nama = data.nama
+console.log(nama)
+
+let warna = ["merah","kuning","hijau"]
+warna.push("coklat")
+console.log(warna)
+let warnaIndexKetiga = warna[2]
+console.log(warnaIndexKetiga)
